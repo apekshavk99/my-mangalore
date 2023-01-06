68d1e960d114cff5cd8ba6203da254919e05fd13
apekshavk <apkvk99@gmail.com>
2023-01-06 11:16:06 +0530

    popup changes changes

--- src/app/festival/festival.component.ts
+++ src/app/festival/festival.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-festival',
@@ -6,42 +7,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./festival.component.scss']
 })
 export class FestivalComponent implements OnInit {
-  ngOnInit(): void {
-    const iframe = document.getElementById("chatbot") as HTMLIFrameElement
-    // document.getElementById("extraIcon").addEventListener("click", function () {
-      window.addEventListener("message", function (msg) {
-        if(msg.origin === "https://dcb2.camiesandbox.com"){
-          var postableEvt = msg && msg.data && msg.data.evtName ? msg.data : null;
-          console.log("msg", msg)
-          console.log("postableEvt", postableEvt)
-          if (postableEvt && iframe) {
-            if(postableEvt.evtName === "loaded"){
-              iframe?.contentWindow?.postMessage({
-                evtName: "iframe",
-                autoLaunch: false,
-              }, "*");
-            }
-            else if (postableEvt.evtName === "chatwindow") {
-              iframe.classList.add("chat-window");
-              iframe.classList.remove("chat-btn");
-              iframe.classList.remove("chat-expand");
-    
-            } else if (postableEvt.evtName == "expand") {
-              iframe.classList.add("chat-expand");
-              iframe.classList.remove("chat-window");
-              iframe.classList.remove("chat-btn");
-            } else {
-              iframe.classList.add("chat-btn");
-              iframe.classList.remove("chat-window");
-              iframe.classList.remove("chat-expand");
-            }
-          }
-          iframe?.contentWindow?.postMessage({
-            call: "pageURL",
-            value: document.location.href
-          }, "*");
-        }
 
-      });
+  userActivity: any;
+  userInactive: Subject<any> = new Subject();
+  idleTime: number = 0
+
+  @HostListener("document:keyup", ["$event"])
+  @HostListener("document:click", ["$event"])
+  @HostListener("document:wheel", ["$event"])
+  @HostListener("document:scroll", ["$event"])
+  @HostListener("document:mousemove", ["$event"])
+  refreshUserState() {
+    clearTimeout(this.userActivity);
+    this.setTimeout();
+  }
+
+  ngOnInit(): void {
+    window.addEventListener("message", this.handleOriantation);
+  }
+
+  setTimeout() {
+    this.userActivity = setTimeout(() => this.userInactive.next(undefined), this.idleTime * 1000);
+  }
+
+  handleOriantation = (msg: any) => {
+    const iframe = document.getElementById("chatbot") as HTMLIFrameElement
+    if (msg.origin === "https://dcb2.camiesandbox.com") {
+      var postableEvt = msg && msg.data && msg.data.evtName ? msg.data : null;
+      console.log("msg", msg)
+      console.log("postableEvt", postableEvt)
+      if (postableEvt && iframe) {
+        if (postableEvt.evtName === "loaded") {
+          iframe?.contentWindow?.postMessage({
+            evtName: "iframe",
+            autoLaunch: false,
+          }, "*");
+        } else if (postableEvt.evtName === "popup-idle") {
+          this.idleTime = Number(postableEvt.behaviour);
+          this.setTimeout();
+          this.userInactive.subscribe(() => {
+            iframe.contentWindow?.postMessage({
+              evtName: "idle-window",
+            }, "*");
+          });
+        }
+        else if (postableEvt.evtName === "chatwindow") {
+          iframe.classList.add("chat-window");
+          iframe.classList.remove("chat-btn");
+          iframe.classList.remove("chat-expand");
+
+        } else if (postableEvt.evtName == "expand") {
+          iframe.classList.add("chat-expand");
+          iframe.classList.remove("chat-window");
+          iframe.classList.remove("chat-btn");
+        } else {
+          iframe.classList.add("chat-btn");
+          iframe.classList.remove("chat-window");
+          iframe.classList.remove("chat-expand");
+        }
+      }
+      iframe?.contentWindow?.postMessage({
+        call: "pageURL",
+        value: document.location.href
+      }, "*");
+    }
+
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,49 +8,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  
+  userActivity: any;
+  userInactive: Subject<any> = new Subject();
+  idleTime: number = 0
+
+
   constructor(){
     window.addEventListener("scroll", this.reveal);
   }
 
-  ngOnInit(): void {
-    const iframe = document.getElementById("chatbot") as HTMLIFrameElement
-    // document.getElementById("extraIcon").addEventListener("click", function () {
-      window.addEventListener("message", function (msg) {
-        if(msg.origin === "https://dcb2.camiesandbox.com"){
-          var postableEvt = msg && msg.data && msg.data.evtName ? msg.data : null;
-          console.log("msg", msg)
-          console.log("postableEvt", postableEvt)
-          if (postableEvt && iframe) {
-            if(postableEvt.evtName === "loaded"){
-              iframe?.contentWindow?.postMessage({
-                evtName: "iframe",
-                autoLaunch: false,
-              }, "*");
-            }
-            else if (postableEvt.evtName === "chatwindow") {
-              iframe.classList.add("chat-window");
-              iframe.classList.remove("chat-btn");
-              iframe.classList.remove("chat-expand");
-    
-            } else if (postableEvt.evtName == "expand") {
-              iframe.classList.add("chat-expand");
-              iframe.classList.remove("chat-window");
-              iframe.classList.remove("chat-btn");
-            } else {
-              iframe.classList.add("chat-btn");
-              iframe.classList.remove("chat-window");
-              iframe.classList.remove("chat-expand");
-            }
-          }
-          iframe?.contentWindow?.postMessage({
-            call: "pageURL",
-            value: document.location.href
-          }, "*");
-        }
-
-      });
+  @HostListener("document:keyup", ["$event"])
+  @HostListener("document:click", ["$event"])
+  @HostListener("document:wheel", ["$event"])
+  @HostListener("document:scroll", ["$event"])
+  @HostListener("document:mousemove", ["$event"])
+  refreshUserState() {
+    clearTimeout(this.userActivity);
+    this.timedOut();
   }
 
+  ngOnInit(): void {
+      window.addEventListener("message", this.handleOriantation);
+  }
+
+
+  handleOriantation = (msg: any) => {
+    const iframe = document.getElementById("chatbot") as HTMLIFrameElement
+    if (msg.origin === "https://dcb2.camiesandbox.com") {
+      var postableEvt = msg && msg.data && msg.data.evtName ? msg.data : null;
+      console.log("msg", msg)
+      console.log("postableEvt", postableEvt)
+      if (postableEvt && iframe) {
+        if (postableEvt.evtName === "loaded") {
+          iframe?.contentWindow?.postMessage({
+            evtName: "iframe",
+            autoLaunch: false,
+          }, "*");
+        } else if (postableEvt.evtName === "popup-idle") {
+          this.idleTime = Number(postableEvt.behaviour);
+          this.timedOut();
+          this.userInactive.subscribe(() => {
+            debugger
+            iframe.contentWindow?.postMessage({
+              evtName: "idle-window",
+            }, "*");
+          });
+        }
+        else if (postableEvt.evtName === "chatwindow") {
+          iframe.classList.add("chat-window");
+          iframe.classList.remove("chat-btn");
+          iframe.classList.remove("chat-expand");
+
+        } else if (postableEvt.evtName == "expand") {
+          iframe.classList.add("chat-expand");
+          iframe.classList.remove("chat-window");
+          iframe.classList.remove("chat-btn");
+        } else {
+          iframe.classList.add("chat-btn");
+          iframe.classList.remove("chat-window");
+          iframe.classList.remove("chat-expand");
+        }
+      }
+      iframe?.contentWindow?.postMessage({
+        call: "pageURL",
+        value: document.location.href
+      }, "*");
+    }
+
+  }
+
+  timedOut() {
+    this.userActivity = setTimeout(() => this.userInactive.next(undefined), this.idleTime * 1000);
+  }
   reveal(){
     let sectionContainer = document.getElementById("sec3");
     if(sectionContainer){
